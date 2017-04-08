b57201e3c402d3fa703fe7c99d882fcd12459a5f
Gowtham Goli <sgg308@nyu.edu>
2017-04-08 18:35:24 -0400

    added particles and new menu

---
+++ Assets/_Scripts/DestroyBolt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBolt : MonoBehaviour {
+
+    public GameObject explosion;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bolt")
+        {
+            GameObject explosionClone = Instantiate(explosion, other.transform.position, other.transform.rotation);
+            //Destroy(explosion.gameObject, 5.0f);
+            Destroy(other.gameObject);
+            Destroy(explosionClone, 2.0f);
+        }
+    }
+}
---
+++ Assets/_Scripts/LoadMultiplayerScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadMultiplayerScene : MonoBehaviour {
+
+	public void load () 
+	{
+		Debug.Log("Pressed multi");
+		SceneManager.LoadScene(1);	
+	}
+}
