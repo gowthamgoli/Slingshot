0404481b03db05ce3136e812614eb153082cefab
Gowtham Goli <sgg308@nyu.edu>
2017-04-09 01:59:42 -0400

    implemented switching cameras

--- Assets/_Scripts/Mover.cs
+++ Assets/_Scripts/Mover.cs
@@ -7,7 +7,11 @@ public class Mover : MonoBehaviour {
 	private Rigidbody rb;
 	public float speed;
 
-	void Start(){
+    public Camera mainCamera;
+    public Camera secondaryCamera;
+
+    void Start(){
+        
 		rb = GetComponent<Rigidbody>();
 		//Debug.Log (transform.right);
 		rb.velocity = transform.right * speed;
---
+++ Assets/_Scripts/SwitchCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour {
+    private Camera mainCamera;
+    private Camera secondaryCamera;
+	// Use this for initialization
+	void Start () {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        secondaryCamera = GameObject.Find("Secondary Camera").GetComponent<Camera>();
+        mainCamera.enabled = true;
+        secondaryCamera.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 posMainCamera = mainCamera.WorldToViewportPoint(transform.position);
+        Vector3 posSecCamera = secondaryCamera.WorldToViewportPoint(transform.position);
+
+        if ((posMainCamera.x < 0 || posMainCamera.x > 1.0f || posMainCamera.y < 0 || posMainCamera.y > 1.0f))
+        {
+            if (!secondaryCamera.enabled)
+            {
+                secondaryCamera.enabled = true;
+                mainCamera.enabled = false;
+            }
+
+            if (posSecCamera.x < 0 || posSecCamera.x > 1.0f || posSecCamera.y < 0 || posSecCamera.y > 1.0f)
+            {
+                if (!mainCamera.enabled)
+                {
+                    mainCamera.enabled = true;
+                    secondaryCamera.enabled = false;
+                }
+            }
+        }
+
+        else if (secondaryCamera.enabled)
+        {
+            mainCamera.enabled = true;
+            secondaryCamera.enabled = false;
+        }
+    }
+}
